45186c723a716285a467044dd4032b1b6889fb0e
Pravin Khadka <Prav.k15@gmail.com>
2023-07-31 20:26:43 -0500

    Admin for Insurance Management System

---
+++ src/main/java/com/takeo/repo/AdminRepository.java
@@ -0,0 +1,19 @@
+package com.takeo.repo;
+
+import java.util.List;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+
+import com.takeo.entity.Admin;
+
+
+public interface AdminRepository extends MongoRepository<Admin, Long> {
+	
+	@Query("select firstname from Admin")
+	List<Admin> getAdminByUsername(Admin admin);
+
+	public Admin verifyUsernameAndPassword(String username, String password);
+	
+
+}
